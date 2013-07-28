14137204ef9e9a19242469210d03e1a4e49d7be0
Jason <jason@yun.io>
2013-07-28 08:09:10 +0800

    merge from_file, from_buffer rename magic_nif to magic

--- c_src/magic.c
+++ c_src/magic.c
@@ -4,60 +4,28 @@
 #include <string.h>
 #include "erl_nif.h"
 
-#define MAXBUFLEN  1024
-
-static ERL_NIF_TERM 
-from_file_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-	char buf[MAXBUFLEN];
-
-    ERL_NIF_TERM errorTerm = enif_make_atom(env, "error");
-    
-    if(enif_get_string(env, argv[0], buf, sizeof(buf), ERL_NIF_LATIN1) < 1) {
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "badarg", ERL_NIF_LATIN1));
-    }
-
-    
-
-    magic_t magic_cookie;
-    //MAGIC_MIME tells magic to return a mime of the file, but you can specify different things
-    // magic_cookie = magic_open(MAGIC_MIME); 
-    magic_cookie = magic_open(MAGIC_MIME_TYPE); 
-    if (magic_cookie == NULL) {
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "magic_open_fail", ERL_NIF_LATIN1));
-    }
-
-     //Loading default magic database
-    if (magic_load(magic_cookie, NULL) != 0) {
-         // cannot load magic database
-        magic_close(magic_cookie);
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "magic_load_database_fail", ERL_NIF_LATIN1));
-    }
-
-    //
-    const char *magic_full;
-    magic_full = magic_file(magic_cookie, buf);
-
-    if(magic_full == NULL){
-        magic_close(magic_cookie);
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, magic_error(magic_cookie), ERL_NIF_LATIN1));
-    }
-
-    ERL_NIF_TERM okTerm = enif_make_atom(env, "ok");
-    ERL_NIF_TERM value = enif_make_string(env, magic_full, ERL_NIF_LATIN1);   
-    magic_close(magic_cookie);
-    return  enif_make_tuple2(env, okTerm, value);
-}
-
+#define FLAG_FROM_FILE 0
+#define FLAG_FROM_BUFFER 1
 
 
 
 static ERL_NIF_TERM 
-from_buffer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+magic_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
     ErlNifBinary p;
-    ERL_NIF_TERM errorTerm = enif_make_atom(env, "error");
+
+     if (argc != 2)
+    {
+        return enif_make_badarg(env);
+    }
 
     if (!enif_inspect_binary(env, argv[0], &p)) {
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "badarg", ERL_NIF_LATIN1));
+        return enif_make_badarg(env);
+    }
+
+     int flag = 0;
+    if (!enif_get_int(env, argv[1], &flag))
+    {
+        return enif_make_badarg(env);
     }
 
     magic_t magic_cookie;
@@ -65,37 +33,71 @@ from_buffer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
     // magic_cookie = magic_open(MAGIC_MIME); 
     magic_cookie = magic_open(MAGIC_MIME_TYPE); 
     if (magic_cookie == NULL) {
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "magic_open_fail", ERL_NIF_LATIN1));
+        return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_atom(env, "fail_to_open_magic"));
     }
 
      //Loading default magic database
     if (magic_load(magic_cookie, NULL) != 0) {
          // cannot load magic database 
         magic_close(magic_cookie);
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, "magic_load_database_fail", ERL_NIF_LATIN1));
+        return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_atom(env, "fail_to_load_magic_database"));
     }
     
     const char *magic_full;
-    magic_full = magic_buffer(magic_cookie, p.data, p.size);
+    switch(flag){
+        case FLAG_FROM_FILE:
+            magic_full = magic_file(magic_cookie, (const char *)p.data);
+            break;
+        case FLAG_FROM_BUFFER:
+            magic_full = magic_buffer(magic_cookie, p.data, p.size);
+            break;
+        default :
+            magic_close(magic_cookie);
+            return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_atom(env, "not_support"));
+    }
 
     if(!magic_full){
         magic_close(magic_cookie);
-        return enif_make_tuple2(env, errorTerm, enif_make_string(env, magic_error(magic_cookie), ERL_NIF_LATIN1));
+        return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_atom(env, magic_error(magic_cookie)));
     }
 
-    ERL_NIF_TERM okTerm = enif_make_atom(env, "ok");
     ERL_NIF_TERM value = enif_make_string(env, magic_full, ERL_NIF_LATIN1);   
     magic_close(magic_cookie);
     
-    return  enif_make_tuple2(env, okTerm, value);
+    return  enif_make_tuple2(env, enif_make_atom(env, "ok"), value);
 }
 
 
 
 static ErlNifFunc nif_funcs[] = {
-    {"from_file", 1, from_file_nif}
-	,{"from_buffer", 1, from_buffer_nif}
+    {"magic", 2, magic_nif}
 };
 
-ERL_NIF_INIT(magic_nif, nif_funcs, NULL, NULL, NULL, NULL)
+static int
+load(ErlNifEnv *env, void **priv, ERL_NIF_TERM info)
+{
+
+    return 0;
+}
+
+static int
+reload(ErlNifEnv *env, void **priv, ERL_NIF_TERM info)
+{
+    return 0;
+}
+
+static int
+upgrade(ErlNifEnv *env, void **priv, void **old_priv, ERL_NIF_TERM info)
+{
+    return load(env, priv, info);
+}
+
+static void
+unload(ErlNifEnv *env, void *priv)
+{
+    return;
+}
+
+// ERL_NIF_INIT(yfs, nif_funcs, NULL, NULL, NULL, NULL);
+ERL_NIF_INIT(magic, nif_funcs, &load, &reload, &upgrade, &unload);
 
---
+++ src/magic.erl
@@ -0,0 +1,72 @@
+-module(magic).
+
+-export([from_file/1]).
+-export([from_buffer/1]).
+
+
+-define(FLAG_FROM_FILE, 0).
+-define(FLAG_FROM_BUFFER, 1).
+
+
+
+-on_load(load_nif/0).
+%% NIF version number (on the load_info argument of load_nif/2)
+-define(NIF_LOAD_INFO, 101).
+
+-define(nif_stub,
+        erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE})).
+
+
+
+%% On-load callback
+
+%% @doc Loading NIF shared library file, used at the on-load callback.
+load_nif() ->
+    PrivDir = case code:priv_dir(?MODULE) of
+          {error, _} ->
+              EbinDir = filename:dirname(code:which(?MODULE)),
+              AppPath = filename:dirname(EbinDir),
+              filename:join(AppPath, "priv");
+          Path ->
+              Path
+          end,
+    erlang:load_nif(filename:join(PrivDir, "magic"), ?NIF_LOAD_INFO).
+
+
+-spec from_file(string() | binary()) -> {ok, string()} | {error, term()}.
+from_file(FilePath) when is_list(FilePath)->
+    from_file(list_to_binary(FilePath));
+from_file(FilePath) when is_binary(FilePath) ->
+    magic(FilePath, ?FLAG_FROM_FILE).
+
+-spec from_buffer(binary()) -> {ok, string()} | {error, term()}.
+from_buffer(Binary) ->
+    magic(Binary, ?FLAG_FROM_BUFFER).
+
+ magic(_Binary, _FLAG) ->
+    ?nif_stub.
+
+
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+
+
+from_file_test() ->
+    FilePath = filename:absname(code:which(?MODULE)),
+    ?assertMatch({ok, _}, from_file(FilePath)).
+
+file_not_found_test() ->
+    FilePath = "/file/not/found",
+    ?assertMatch({error, _}, from_file(FilePath)).
+
+from_buffer_test() ->
+    FilePath = filename:absname(code:which(?MODULE)),
+    Size=64,
+    {ok, File} = file:open(FilePath, [read,binary]),
+    {ok, FileBinary} =  file:read(File, Size), 
+    file:close(File), 
+    ?assertMatch({ok, _}, from_buffer(FileBinary)).
+-endif.
